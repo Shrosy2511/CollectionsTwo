b5a36c85b4731b6232bb0ddf4bb1afd6ed48459c
Shrosy2511 <99042114@mydavinci.nl>
2021-12-10 19:07:02 +0100

    finished ww generator

--- passwordGenerator.py
+++ passwordGenerator.py
@@ -1,10 +1,19 @@
 import random
 
-choice = [random.choice('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ') for i in range(3)]
-choice1 = [random.choice('1234567890') for i in range(7)]
-choice2 = [random.choice('!@#$%^&*()') for i in range(3)]
-choice3 = [random.choice('abcdefghijklmnopqrstuvwxyz') for i in range(8)]
-choice4 = [random.choice('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890') for i in range(3)]
-choices = choice1+choice2+choice3
-random.shuffle(choices)
-print('hier is uw wachtwoord met 24 characters:',''.join(choice+choices+choice4))
+choice = [random.choice('abcdefghijklmnopqrstuvwxyz') for i in range(2)]
+choice1 = [random.choice('ABCDEFGHIJKLMNOPQRSTUVWXYZ') for i in range(random.randint(1,3))]
+choice2 = [random.choice('1234567890')for i in range(random.randint(4,7))]
+choice3 = [random.choice('!@#$%^&*()')for i in range(random.randint(3, 3))]
+
+choice4 = choice+choice1
+random.shuffle(choice4)
+choice5 = choice+choice1+choice2+choice3
+random.shuffle(choice5)
+choice6 = choice4+choice5
+choice8 = 24-len(choice6)
+choice7 = [random.choice('abcdefghijklmnopqrstuvwxyz') for i in range(choice8)]
+if len(choice6) < 24:
+    print(''.join(choice6+choice7))
+    print('aantal characters:', len(choice6+choice7))
+else:
+    print(choice6)
