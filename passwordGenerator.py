3d016244fa5591da05a28d0aa70f676580575779
Shrosy2511 <99042114@mydavinci.nl>
2021-12-10 19:54:02 +0100

    added ww generator

--- passwordGenerator.py
+++ passwordGenerator.py
@@ -1,19 +1,19 @@
 import random
 
-choice = [random.choice('abcdefghijklmnopqrstuvwxyz') for i in range(2)]
-choice1 = [random.choice('ABCDEFGHIJKLMNOPQRSTUVWXYZ') for i in range(random.randint(1,3))]
-choice2 = [random.choice('1234567890')for i in range(random.randint(4,7))]
-choice3 = [random.choice('!@#$%^&*()')for i in range(random.randint(3, 3))]
+choiceLC = [random.choice('abcdefghijklmnopqrstuvwxyz') for i in range(2)]
+choiceUP = [random.choice('ABCDEFGHIJKLMNOPQRSTUVWXYZ') for i in range(random.randint(1,3))]
+choiceNUM = [random.choice('1234567890')for i in range(random.randint(4,7))]
+choiceSYM = [random.choice('!@#$%^&*()')for i in range(3)]
 
-choice4 = choice+choice1
-random.shuffle(choice4)
-choice5 = choice+choice1+choice2+choice3
-random.shuffle(choice5)
-choice6 = choice4+choice5
-choice8 = 24-len(choice6)
-choice7 = [random.choice('abcdefghijklmnopqrstuvwxyz') for i in range(choice8)]
-if len(choice6) < 24:
-    print(''.join(choice6+choice7))
-    print('aantal characters:', len(choice6+choice7))
+choice_LW_UP = choiceLC+choiceUP
+random.shuffle(choice_LW_UP)
+choiceALL = choiceLC+choiceUP+choiceNUM+choiceSYM
+random.shuffle(choiceALL)
+choiceTotall = choice_LW_UP+choiceALL
+choiceSUM = 24-len(choiceTotall)
+choiceADD = [random.choice('abcdefghijklmnopqrstuvwxyz') for i in range(choiceSUM)]
+if len(choiceTotall) < 24:
+    print(''.join(choiceTotall+choiceADD))
+    print('aantal characters:', len(choiceTotall+choiceADD))
 else:
-    print(choice6)
+    print(choiceTotall)
