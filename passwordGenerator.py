3df308efc04e35b127c8f635962a1c8291a183cb
Shrosy2511 <99042114@mydavinci.nl>
2021-12-10 15:10:09 +0100

    wachtwoord generatie

---
+++ passwordGenerator.py
@@ -0,0 +1,10 @@
+import random
+
+choice = [random.choice('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ') for i in range(3)]
+choice1 = [random.choice('1234567890') for i in range(7)]
+choice2 = [random.choice('!@#$%^&*()') for i in range(3)]
+choice3 = [random.choice('abcdefghijklmnopqrstuvwxyz') for i in range(8)]
+choice4 = [random.choice('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890') for i in range(3)]
+choices = choice1+choice2+choice3
+random.shuffle(choices)
+print('hier is uw wachtwoord met 24 characters:',''.join(choice+choice1+choice2+choice3+choice4))
